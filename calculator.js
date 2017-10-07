bdd315f8b4d40af461f017c8b4dffeaf2d7dc532
kdluu <khanhluu101@gmail.com>
2017-10-07 18:12:32 -0500

    Finish buttons. Add button value to scree.

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 // CALCULATOR.JS
-//   
+//
 //
 //
 
@@ -14,15 +14,34 @@ class Calculator {
           textRow : {id: "textRow", type: "text", value: "", onclick:""},
           button7 : {id: "button7", type: "button", value: 7, onclick:""},
           button8 : {id: "button8", type: "button", value: 8, onclick:""},
+          button9 : {id: "button9", type: "button", value: 9, onclick:""},
+          addittion : {id: "addittion", type: "button", value: '&#43', onclick:""},
+
+          button4 : {id: "button4", type: "button", value: 4, onclick:""},
+          button5 : {id: "button5", type: "button", value: 5, onclick:""},
+          button6 : {id: "button6", type: "button", value: 6, onclick:""},
+          substraction : {id: "substraction", type: "button", value:'&#45', onclick:""},
+
+          button1 : {id: "button1", type: "button", value: 1, onclick:""},
+          button2 : {id: "button2", type: "button", value: 2, onclick:""},
+          button3 : {id: "button3", type: "button", value: 3, onclick:""},
+          multiply : {id: "multiply", type: "button", value:'&#42', onclick:""},
+
+          button0 : {id: "button0", type: "button", value: 0, onclick:""},
+          button5 : {id: "button7", type: "button", value: 5, onclick:""},
+          button6 : {id: "button8", type: "button", value: 6, onclick:""},
+          substraction : {id: "substraction", type: "button", value:'&#45', onclick:""},
+
+
+
+
           container : document.getElementById(elementId)
         };
 
         this.Controller = {
           viewClickHandler : function(e) {
              let target = e.target;
-             if (target.id == "button7") {
-               this.button7Handler();
-             }
+             this.buttonHandler(target.value);//Value of button
           }
         };
 
@@ -49,11 +68,12 @@ class Calculator {
 
 
     //
-    // button7Handler
+    // Passing value of button to buttonHandler
     //
-    button7Handler() {
-      alert("Hi this is button7 in " + this.View.container.id);
+    buttonHandler(value) {
+      document.getElementById("textRow").value += value;
     }
+
 
     //
     // createHTMLforView
@@ -70,6 +90,25 @@ class Calculator {
       // thisulator buttons
       s += this.createHTMLforElement(this.View.button7);
       s += this.createHTMLforElement(this.View.button8);
+      s += this.createHTMLforElement(this.View.button9);
+      s += this.createHTMLforElement(this.View.addittion);
+      s += "<br>";
+      s += this.createHTMLforElement(this.View.button4);
+      s += this.createHTMLforElement(this.View.button5);
+      s += this.createHTMLforElement(this.View.button6);
+      s += this.createHTMLforElement(this.View.substraction);
+      s += "<br>";
+      s += this.createHTMLforElement(this.View.button1);
+      s += this.createHTMLforElement(this.View.button2);
+      s += this.createHTMLforElement(this.View.button3);
+      s += this.createHTMLforElement(this.View.multiply);
+      s += "<br>";
+      s += this.createHTMLforElement(this.View.button0);
+      s += this.createHTMLforElement(this.View.button5);
+      s += this.createHTMLforElement(this.View.button6);
+      s += this.createHTMLforElement(this.View.substraction);
+
+
       s += "</tr></td></table>";
       return s;
     }
@@ -88,5 +127,6 @@ class Calculator {
       s += ">";
       return s;
     }
+
 
 } // end of Calculator;
